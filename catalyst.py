a2644c4eaffbec2b2ba71843b96fa8c7c30eafd4
Anna Ollenschleger <ago@Annas-MacBook-Air.local>
2024-04-19 16:25:35 -0400

    final adjustments of project

--- catalyst.py
+++ catalyst.py
@@ -1,22 +1,35 @@
 from bokeh.plotting import figure, show
-import numpy as np
+from bokeh.models import ColumnDataSource
+from bokeh.transform import factor_cmap
 import math
 class jazz:
-    f = figure()
+     f = figure(title = "Creativity Through Performace in Jazz Improvisation", width = 1000)
 
-    x = [-204.70, -192.12, -191.52, -191.41]
-    y = [0.0788, 0.002, 0.0271, 0.489]
-    f.circle(x, y, size = 8, color = "green")
-    show(f)
+     x = [-204.70, -203.80, -192.12, -191.52, -191.41]
+     y = [0.0788, 0.171, 0.002, 0.0271, 0.489]
+     legend = ["Music Training", "Jazz Training", "With Experience", "With Instructions", "With Instructions and Experience"]
+
+     f.circle(x[0], y[0], size = 10, color = "green", legend_label = legend[0])
+     #music training
+     f.circle(x[1], y[1], size = 10, color = "red", legend_label = legend[1])
+     #jazz training
+     f.circle(x[2], y[2], size = 10, color = "blue", legend_label = legend[2])
+     #experience
+     f.circle(x[3], y[3], size = 10, color = "purple", legend_label = legend[3])
+     #instructions
+     f.circle(x[4], y[4], size = 10, color = "turquoise", legend_label = legend[4])
+     #instructions + experience
+    
+     f.legend.orientation = "horizontal"
+     f.legend.location = "top_center"
+    
+     show(f)
 
 class school:
      data = [46, 59, 71, 80]
      names = ["Performs Well on Standarized Tests", "Deep Understanding of Classroom Concepts", "Making Connections to Multiple Subjects", "Shows Problem Solving"]
-     f2 = figure(x_range = names, height = 500, width = 1000, title = "Percentages of Students with High Creativity's Performace in School")
-     f2.vbar(x = names, top = data, width = .6, color = "pink")
-
-     f2.xgrid.grid_line_color = None
-     f2.y_range.start = 0
+     f2 = figure(x_range = names, height = 500, width = 1000, title = "Students with High Creativity Performace in School by Percent")
+     f2.vbar(x = names, top = data, width = .6, color = "magenta")
 
      show (f2)
 
